bf1364e2ab4f897ac1ecb2325392163cea5e7e6d
Bogdan Didukh <bdidukh@tsukat.com>
2021-09-05 13:48:15 +0300

    Add console output

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
 
 namespace MergeSortRealization
 {
@@ -12,18 +14,25 @@ namespace MergeSortRealization
             DESC 
         }
 
-        private static string pattern = ",";
+        private const char Separator = ',';
+        
         private static SortOrder _sortOrder;
         private static int[] _inputArray;
         private static int[] _outputArray;
         private static bool  _isValidFirstArg;
-
+        private static Stopwatch _stopwatch;
+        private static long _time;
+        private static int _comparisonsCounter = 0;
+        private static int _swapsCounter = 0;
+        private static string _output = new string(string.Empty);
+        
         public static void Main(string[] args)
         {
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+
             _isValidFirstArg = Enum.TryParse(args[0], out _sortOrder);
-            //string[] temp = Regex.Split(args[1], pattern);
-            //int[] _inputArray = Array.ConvertAll(Regex.Split(args[1], pattern), s => int.Parse(s));
-            _inputArray = args[1].Split(',').Select(int.Parse).ToArray();
+            _inputArray = args[1].Split(Separator).Select(int.Parse).ToArray();
             _outputArray = new int[_inputArray.Length];
 
             if (!_isValidFirstArg)
@@ -31,12 +40,35 @@ namespace MergeSortRealization
                 Console.WriteLine("INVALID COMMAND");
                 throw new ArgumentException();
             }
+            
             MergeSort();
+            
+            _stopwatch.Stop();
+
+            _time = _stopwatch.ElapsedMilliseconds;
+            
+            ConsoleOutput();
+        }
+
+        private static void ConsoleOutput()
+        {
+            Console.WriteLine("MergeSort:");
+            Console.WriteLine(_time + "ms");
+            Console.WriteLine("Comparisons:" + _comparisonsCounter);
+            Console.WriteLine("Swaps:" + _swapsCounter);
+            foreach (var item in _outputArray)
+            {
+                _output += item;
+                _output += Separator;
+            }
+            _output = _output.TrimEnd(Separator);
+            
+            Console.WriteLine(_output);
         }
 
         private static void MergeSort()
         {
-            Console.WriteLine("Success");
+            _outputArray = _inputArray;
         }
     }
 }
